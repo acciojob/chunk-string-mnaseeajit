0f0c0bfec1339b41f32fd1372f91c42b96dcb21f
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2023-12-12 14:15:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function stringChop(str, size) {
   // your code here
+	if (size <= 0) {
+    throw new Error("Chunk length should be greater than 0");
+  }
+
+  const result = [];
+  let i = 0;
+
+  while (i < str.length) {
+    result.push(str.slice(i, i + size));  
+    i += size;
+  }
+
+  return result;
 }
 
 // Do not change the code below
